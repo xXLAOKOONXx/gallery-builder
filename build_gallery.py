be6328638d3adda438c8a138521484df8fc3a3d2
unknown <tom.eckstein@web.de>
2024-03-19 20:53:09 +0100

    Remove style link

--- build_gallery.py
+++ build_gallery.py
@@ -11,7 +11,6 @@ HTML_HEADER = '''
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="./style.css">
 
   <title>GALLERY</title>
 </head>
